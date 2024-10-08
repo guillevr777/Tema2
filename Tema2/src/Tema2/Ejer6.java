98dab4a106c2272c1a4e33b1cb8347049fb4ba4a
gs.villanueva <gs.villanueva@106-29.info2>
2024-10-08 14:21:06 +0200

    Update exercise 6

---
+++ Tema2/src/Tema2/Ejer6.java
@@ -0,0 +1,38 @@
+package Tema2;
+
+import java.util.Scanner;
+
+public class Ejer6 {
+	public static void main(String[]args) {
+	//creamos la consola
+	Scanner reader = new Scanner(System.in);
+	//creamos las variables
+	int a;
+	int b;
+	int c;
+	//creamos variables para las soluciones
+	double x1;
+	double x2;
+	double discriminante;
+	System.out.println("introduzca un coeficiente de  grado");
+	a=reader.nextInt();
+	b=reader.nextInt();
+	c=reader.nextInt();
+	
+	//si la  a==0
+	if (a==0) {
+		x1 = -c/b;
+			System.out.println("a");
+	} else {
+		discriminante = b*b-4*a*c;
+		if (discriminante < 0) {
+			System.out.println("a");
+		} else {
+				x1 = (-b + Math.sqrt(discriminante)) / (2*a);
+				x2 = (-b - Math.sqrt(discriminante)) / (2*a);
+				System.out.println(x1);
+				System.out.println(x2);
+			}	
+		}
+	}
+}
