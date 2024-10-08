a8eb0fc3503e6cee8db31f730a712bb9538105dc
gs.villanueva <gs.villanueva@106-29.info2>
2024-10-08 14:38:43 +0200

    a

--- Tema2/src/Tema2/Ejer9.java
+++ Tema2/src/Tema2/Ejer9.java
@@ -1,5 +1,24 @@
 package Tema2;
 
-public class Ejer9 {
+import java.util.Scanner;
 
+public class Ejer9 {
+	public static void main(String[]args) {
+		//creamos la consola
+		Scanner reader = new Scanner(System.in);
+		Object jugador;
+		//creamos las variables
+		String jugador1;
+		String jugador2;
+		System.out.println("jugador 1 , elige entre piedra papel o tijeras");
+		jugador1=reader.next();
+		System.out.println("jugador 2 , elige entre piedra papel o tijeras");
+		jugador2=reader.next();
+		if (jugador1.equals(jugador2)) {
+		}else if (jugador1.equals(piedra)) && jugador2.equals(tijeras)
+				||(jugador1.equals(papel)) && jugador2.equals(piedra)
+				||(jugador1.equals(tijeras)) && jugador2.equals(papel)
+			}
+		}
+	}
 }
