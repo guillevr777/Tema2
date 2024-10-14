23fc677df43538070863fd3dc0296648ea28b9b8
gs.villanueva <gs.villanueva@106-29.info2>
2024-10-14 12:12:38 +0200

    Correcting

--- Tema2/src/partetres/Ejer4.java
+++ Tema2/src/partetres/Ejer4.java
@@ -14,16 +14,41 @@ public class Ejer4 {
 		
 		//creamos las variables
 		int numero;
+		int unidades;
+		
+		//cadena guardar resultado
+		String resul = "";
 		
 		//le preguntamos por un numero de 8 cifras
 		System.out.println("Dime un numero entre 0 y 99.");
 		numero = reader.nextInt();
 		
 		//Usamos if else para saber si el numero cumple los requisitos
-		if () {	
-			
-			} else if (numero < 0 && numero > 99) { 
-				System.out.println("Tu numero no cumple los requisitos para asignarle una letra.");
+		switch (numero) {
+		case 10 -> resul = "diez");
+		case 11 -> resul = "once");
+		case 12 -> resul = "doce");
+		case 13 -> resul = "trece");
+		case 14 -> resul = "catorce");
+		case 15 -> resul = "quince");
+			default -> {
+				
+				switch (unidades) {
+				case 1 -> resul = "uno";
+				case 1 -> resul = "uno";
+				case 1 -> resul = "uno";
+				case 1 -> resul = "uno";
+				}
+				
+				switch (decenas) {
+				case 1: resul = "dieci" + resul;
+				case 1: resul = "veinti" + resul;
+				case 1: resul = "treinta y" + resul;
+				case 1: resul = "uno";
+				case 1: resul = "uno";
+				}
+			}
+
 		}
 	//cerramos el escaner
 	reader.close();
