310f0ce2f6a94fa6d38caf8b1db4a3bc2654528b
guillevr777 <gs.villanueva@iesnervion.es>
2024-10-22 14:39:44 +0200

    doing it

---
+++ Tema2/src/parteseis/Ejer2.java
@@ -0,0 +1,26 @@
+package parteseis;
+
+import java.util.Scanner;
+
+public class Ejer2 {
+public static void main(String[] args) {
+		
+		//creamos el escaner
+		Scanner reader = new Scanner(System.in);
+		
+		//creamos la variable
+		int numero;
+		
+		//preguntamos por el valor de la variable
+		System.out.println("Dime un numero");
+		numero = reader.nextInt();
+		
+		//hacemos el for
+		for (int numeroDos = 0 ; numeroDos<=numero ; numeroDos++ ) {
+			System.out.print(numeroDos + " ; ");
+		}
+	//cerramos el escaner
+	reader.close();
+	}
+}
+}
