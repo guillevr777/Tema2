eb41e218c8fdd5bdbf1b8bdc8e56e3242967ef53
guillevr777 <gs.villanueva@iesnervion.es>
2024-10-28 12:52:12 +0100

    a

--- Tema2/src/partedebucles/Ejer10.java
+++ Tema2/src/partedebucles/Ejer10.java
@@ -10,12 +10,40 @@ public class Ejer10 {
 		
         int numero;
         int digito;
+        int primerD;
+        int segundoD;
+        int tercerD;
+        int cuartoD;
+        int quintoD;
+        int sextoD;
         
         System.out.println("dime el numero");
         numero = reader.nextInt();
 
         while (numero > 0) {
+        	
             digito = numero % 10;
-        }
-    }
+          
+            if (digito < 10) {
+            	primerD = digito;
+            } 
+            if (digito < 100 && digito >= 10) {
+            	segundoD = digito;
+            }
+            if (digito < 1000 && digito >= 100) {
+            	tercerD = digito;
+            }
+            if (digito < 10000 && digito >= 1000) {
+            	cuartoD = digito;
+            }
+            if (digito < 100000 && digito >= 10000) {
+            	quintoD = digito;
+            }
+            if (digito < 1000000 && digito >= 100000) {
+            	sextoD = digito;
+            }
+        } 
+        //cerramos el escaner
+        reader.close();
+	}
 }
